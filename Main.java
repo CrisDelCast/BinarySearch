e0cc6b0930733741fe2ef403bc795fe8db8e236b
CrisDelCast <72984871+CrisDelCast@users.noreply.github.com>
2021-03-19 19:01:18 -0500

    online judge aprobed

---
+++ Main.java
@@ -0,0 +1,39 @@
+package ui;
+import java.util.Arrays;
+import java.util.Scanner;
+ 
+class Main {
+	
+    public static void main(String[] args) {
+        @SuppressWarnings("resource")
+        
+		Scanner in = new Scanner(System.in);
+        
+        while (in.hasNext()) {
+        	
+            int n = in.nextInt();
+            int[] a = new int[n];
+            
+            	for (int i = 0; i < n; i++)
+            		a[i] = in.nextInt();
+            		Arrays.sort(a);
+            int money = in.nextInt();
+            int x = -1;
+            int y = -1;
+            	for (int i = 0; i < n; i++) {
+            		int req = money - a[i];
+            		int res = Arrays.binarySearch(a,i+1,n,req);
+                if (res >= 0 && (a[res] - a[i] < y - x || (x == -1 && y == -1))) {
+                    x = a[i];
+                    y = a[res];
+                }
+            	}
+            System.out.printf("Peter should buy books whose prices are %d and %d.\n\n",x,y);
+            
+        }
+        
+    }
+}
+    
+
+
